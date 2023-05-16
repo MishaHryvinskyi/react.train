dabcdaf5a99a5b4fd7cf6d0f1d8946d5a8f944d6
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-05-16 17:35:03 +0300

    create React

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+// REACT
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// const h1 = React.createElement('h1', {id:"1"}, "Катерина");
+// root.render(h1);
+// console.log(h1);
+
+// JS
+// const root = document.querySelector("#root");
+// const h1 = document.createElement("h1");
+// h1.textContent = "Привіт Кулюрина!!!"
+// console.dir(h1);
+// root.prepend(h1);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
