24683a61a758e51099599e0268d1a1c66e98fb40
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-05-22 16:32:44 +0300

    del old

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,15 @@
 // import clsx from "clsx";
-import { PageTitle } from '../PageTitle/PageTitle';
-import { EventBoard } from '../EventBoard/EventBoard';
-import upcomingsEvents from '../../upcoming-events.json';
+
+
+
 import { Container } from './App.styled';
-import Counter from 'components/constants/Counter/Counter';
+
 
 
 function App() {
   return (
     <Container>
-      <PageTitle text="24ht Core Worlds Coalition Conference"/>
-      <EventBoard events={upcomingsEvents}/>
-      <Counter initialValue={0}/>
+     <button>button</button>
     </Container>
   );
 }
--- src/components/constants/iconSize.js
+++
@@ -1,9 +0,0 @@
-export const iconSize = Object.freeze({
-    sm: 16,
-    md: 24,
-    lg: 32,
-});
-
-export const iconColor = Object.freeze({
-    red: 'red',
-})
