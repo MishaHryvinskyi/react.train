a0ee289afcc550b5db119b8243c3fd46a1ce6942
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-05-29 13:58:45 +0300

    input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,19 @@
 import React, {Component} from "react";
-import { Form } from './Form.styled';
+import { Form, Input, Btn } from './Form.styled';
 
 class MyForm extends Component {
     state = {
         name: '',
         tag: '', 
+        experience: 'junior',
+        licence: false,
       }
     
-    
+    handleLicenceChenge = e => {
+      console.log(e.currentTarget.checked)
+      this.setState({ licence: e.currentTarget.checked })
+    }
+
     handleChange = event => {
       const { name, value } = event.currentTarget;
     
@@ -30,20 +36,56 @@ render() {
     return (
         <Form onSubmit={this.handleSubmit}>
         <label/>
-        ім'я  <input 
+        ім'я  <Input 
       type='text'
       name='name' 
       value={this.state.name}
       onChange={this.handleChange} 
       />
       <label/>
-      Прізвище <input 
+      Прізвище <Input 
       type='text'
       name='tag'
       value={this.state.tag}
       onChange={this.handleChange} 
       />
-      <button type='submit'>Відправити</button>
+
+      <p>Ваш рівень:</p>
+      <label>
+      <input 
+      type="radio" 
+      name="experience" 
+      value="junior" 
+      onChange={this.handleChange}
+      checked={this.state.experience === 'junior'} />junior
+      </label>
+      <label>
+      <input 
+      type="radio" 
+      name="experience" 
+      value="middle" 
+      onChange={this.handleChange} 
+      checked={this.state.experience === 'middle'}/>middle
+      </label>
+      <label>
+      <input 
+      type="radio" 
+      name="experience" 
+      value="senior" 
+      onChange={this.handleChange} 
+      checked={this.state.experience === 'senior'}/>senior
+      </label>
+
+      <label>
+      <input 
+      type="checkbox" 
+      name="licence" 
+      checked={this.state.licence} 
+      onChange={this.handleLicenceChenge} 
+      /> Погоджуюся на умови
+      </label>
+
+      <Btn type='submit' disabled={!this.state.licence} >Відправити</Btn>
       </Form>
     )
 }
--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -3,4 +3,15 @@ import styled from "@emotion/styled";
 export const Form = styled.form`
     display: flex;
     flex-direction: column;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const Input = styled.input`
+    width: 200px;
+    margin-bottom: 20px;
+`;
+
+export const Btn = styled.button`
+    width: 150px;
 `;
